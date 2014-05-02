b3bc5ff3c2f5b3e3ca831bc3ff71bc291b389f87
xq8922 <xq8922@gmail.com>
2014-05-02 23:36:39 +0800

    Revert "ldpc"
    This reverts commit 52f9f3816f00723cec9facce56cb5ad261b53c0b.

--- helloworld/test.cpp
+++ helloworld/test.cpp
@@ -1,26 +1,7 @@
 #include <iostream>
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include <stdlib.h>
-
-#define direc "E:\\workstation\\workstation\\sumProductDecoding\\H.txt"
-#define PI 3.1415
-#define direc "E:\\workstation\\workstation\\sumProductDecoding\\H.txt"
-#define NUM_1 12
-#define NUM_2 20
-#define NUM_3 200
-#define NUM_4 330
-#define CODE_LEN 156
-#define SIGMA 1
-
-
-int N , M ;      // size of the matrix
-int biggest_num_n,biggest_num_m;
-int nlist[CODE_LEN][CODE_LEN],mlist[CODE_LEN][CODE_LEN];
 
 int main()
 {
-
+    std::cout << "hello world!" << std::endl;
     return 0;
 }
